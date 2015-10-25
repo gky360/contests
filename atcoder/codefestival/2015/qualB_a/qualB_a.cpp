888f3e5498ce6adb056d7f0bb51500bf74def902
scarletrunner7000 <scarletrunner7000@gmail.com>
2015-10-26 00:21:11 +0900

    [AC][codefestival2015qualB_a] ダブル文字列/Double String
    * 与えられた文字列を2回繰り返して出力するだけ

---
+++ atcoder/codefestival/2015/qualB_a/qualB_a.cpp
@@ -0,0 +1,21 @@
+// CODE FESTIVAL 2015 予選B A - ダブル文字列/Double String
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+#define MAX_S 10
+
+
+char str[MAX_S + 1];
+
+int main() {
+
+  scanf("%s", str);
+  printf("%s%s\n", str, str);
+
+  return 0;
+
+}
+
+
