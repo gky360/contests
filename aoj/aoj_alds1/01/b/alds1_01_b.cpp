7be1f486ca6e2a12fb0ae9498979a9c7df8927ec
scarletrunner7000 <scarletrunner7000@gmail.com>
2015-10-26 22:53:01 +0900

    [AC][aoj_alds1_01_b] Greatest Common Divisor
    * ユークリッドの互除法の練習
    
    <ユークリッドの互除法>

---
+++ aoj/aoj_alds1/01/b/alds1_01_b.cpp
@@ -0,0 +1,28 @@
+// Algorithms and Data Structures I
+// 1 - Getting Started
+// B - Greatest Common Divisor
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+
+int gcd(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
+int main() {
+
+  int a, b;
+
+  scanf("%d%d", &a, &b);
+  printf("%d\n", gcd(a, b));
+
+  return 0;
+
+}
+
+
