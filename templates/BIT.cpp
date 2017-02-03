23f243d284d7bd0cc686d5b95c61770bc063f592
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-02-04 01:58:08 +0900

    LinearBIT をテンプレに追加

--- templates/BIT.cpp
+++ templates/BIT.cpp
@@ -45,7 +45,40 @@ void BIT<T>::add(int i, T x) {
 }
 
 
+template <typename T>
+class LinearBIT {
+public:
+  BIT<T> bit0, bit1;
+  void init(int num);
+  T sum(int i);
+  void add(int l, int r, T x); // [l, r)
+};
+
+template <typename T>
+void LinearBIT<T>::init(int num) {
+  bit0.init(num);
+  bit1.init(num);
+  return;
+}
+
+// [l, r)
+template <typename T>
+void LinearBIT<T>::add(int l, int r, T x) {
+  bit0.add(l, -x * (l - 1));
+  bit1.add(l, x);
+  bit0.add(r, x * (r - 1));
+  bit1.add(r, -x);
+  return;
+}
+
+template <typename T>
+T LinearBIT<T>::sum(int i) {
+  return bit1.sum(i) * i + bit0.sum(i);
+}
+
+
 BIT<int> bit;
+LinearBIT<int> rb;
 
 int main(){
 
