bbe200d3769efa13eaf18a8daad69c97390bc4ed
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-04-26 21:09:23 +0900

    [AC][ABC057] A - Remaining Time

---
+++ atcoder/abc/057/a/a.cpp
@@ -0,0 +1,20 @@
+// AtCoder Beginner Contest 057
+// A - Remaining Time
+
+#include <cstdio>
+
+using namespace std;
+typedef long long int ll;
+
+int A, B;
+
+int main() {
+
+  scanf("%d%d", &A, &B);
+  printf("%d\n", (A + B) % 24);
+
+  return 0;
+
+}
+
+
