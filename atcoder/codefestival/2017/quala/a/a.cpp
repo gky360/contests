90f018cc092ab4b98ed4e29796db6f8b7f10e195
gky360 <gky360@gmail.com>
2018-01-28 02:08:44 +0900

    [code-festival-2017-quala] A - Snuke's favorite YAKINIKU

---
+++ atcoder/codefestival/2017/quala/a/a.cpp
@@ -0,0 +1,21 @@
+// CODE FESTIVAL 2017 qual A
+// A - Snuke's favorite YAKINIKU
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+
+
+string S;
+
+int main() {
+
+    cin >> S;
+
+    cout << (S.find("YAKI") == 0 ? "Yes" : "No") << endl;
+
+    return 0;
+
+}
