08b2460ff9f402727a574abbe3cfd0ebd70ac772
gky360 <gky360@gmail.com>
2018-03-25 23:57:23 +0900

    [ABC089] A - Grouping 2

---
+++ atcoder/abc089/a/a.cpp
@@ -0,0 +1,17 @@
+// ABC089
+// A - Grouping 2
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+
+int N;
+
+int main() {
+    cin >> N;
+    cout << N / 3 << endl;
+
+    return 0;
+}
