e97aa77b372039de6122bff9c13bec6a34a1b57f
gky360 <gky360@gmail.com>
2019-01-22 11:51:52 +0900

    [abc103] C - Modulo Summation

---
+++ atcoder/abc103/c/Main.cpp
@@ -0,0 +1,46 @@
+/*
+[abc103] C - Modulo Summation
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+
+const int MAX_N = 3000;
+
+int N;
+int a[MAX_N];
+
+ll solve() {
+    ll ret = 0;
+    for (int i = 0; i < N; i++) {
+        ret += a[i] - 1;
+    }
+    return ret;
+}
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    cout << solve() << endl;
+
+    return 0;
+}
