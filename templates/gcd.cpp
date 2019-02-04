081fa050f302894a72201a38128588c9692d4d98
gky360 <gky360@gmail.com>
2019-02-04 19:07:40 +0900

    fix template

--- templates/gcd.cpp
+++ templates/gcd.cpp
@@ -1,12 +1,18 @@
 // gcd.cpp
 #include <algorithm>
 using namespace std;
+typedef long long int ll;
 
 int gcd(int a, int b) {
     while (b > 0) {
         swap(b, a %= b);
     }
     return a;
+}
+
+ll lcm(int a, int b) {
+    int d = gcd(a, b);
+    return (ll)a * b / d;
 }
 
 int extgcd(int a, int b, int& x, int& y) {
