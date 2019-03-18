989ac8d5332ea22af6b9726da4dfbf37248854c4
gky360 <gky360@gmail.com>
2019-03-18 19:13:19 +0900

    [code-festival-2018-qualb] E - Game of +-

---
+++ atcoder/code-festival-2018-qualb/e/Main.cpp
@@ -0,0 +1,36 @@
+/*
+[code-festival-2018-qualb] E - Game of +-
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+
+
+ll solve() {
+
+}
+
+int main() {
+
+
+    cout << solve() << endl;
+
+    return 0;
+
+}
