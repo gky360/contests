b0d95a7c33903623e35738fbbd2ea6ba215aaf13
gky360 <gky360@gmail.com>
2019-05-24 23:38:44 +0900

    fix RMQ template

--- templates/RMQ.cpp
+++ templates/RMQ.cpp
@@ -13,54 +13,35 @@ class RMQ {
 public:
     int n;
     vector<T> dat;
-    void init(int _n);
-    void update(int k, T a);
-    T query(int a, int b);
+    void init(int _n) {
+        n = 1;
+        while (n < _n) {
+            n <<= 1;
+        }
+        dat.reserve(2 * n - 1);
+        fill(dat.begin(), dat.end(), INF);
+    }
+    void update(int k, T a) {
+        k += n - 1;
+        dat[k] = a;
+        while (k > 0) {
+            k = (k - 1) / 2;
+            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+    T query(int a, int b) { return query(a, b, 0, 0, n); }
 
 private:
-    T query(int a, int b, int k, int l, int r);
+    T query(int a, int b, int k, int l, int r) {  // [a, b), [l, r)
+        if (r <= a || b <= l) {
+            return INF;
+        }
+        if (a <= l && r <= b) {
+            return dat[k];
+        }
+        return min(query(a, b, k * 2 + 1, l, (l + r) / 2),
+                   query(a, b, k * 2 + 2, (l + r) / 2, r));
+    }
 };
-
-template <typename T>
-void RMQ<T>::init(int _n) {
-    n = 1;
-    while (n < _n) {
-        n <<= 1;
-    }
-    dat.reserve(2 * n - 1);
-    for (int i = 0; i < 2 * n - 1; i++) {
-        dat[i] = INF;
-    }
-    return;
-}
-
-template <typename T>
-void RMQ<T>::update(int k, T a) {
-    k += n - 1;
-    dat[k] = a;
-    while (k > 0) {
-        k = (k - 1) / 2;
-        dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
-    }
-    return;
-}
-
-template <typename T>
-T RMQ<T>::query(int a, int b) {
-    return query(a, b, 0, 0, n);
-}
-
-// [a, b), [l, r)
-template <typename T>
-T RMQ<T>::query(int a, int b, int k, int l, int r) {
-    if (r <= a || b <= l) {
-        return INF;
-    }
-    if (a <= l && r <= b) {
-        return dat[k];
-    }
-    return min(query(a, b, k * 2 + 1, l, (l + r) / 2),
-               query(a, b, k * 2 + 2, (l + r) / 2, r));
-}
 
 int main() { return 0; }
