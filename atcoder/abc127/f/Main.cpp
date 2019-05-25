6659896215b1781c124aefc7c5be4807d2ccd5f3
gky360 <gky360@gmail.com>
2019-05-26 01:49:13 +0900

    [abc127] F - Absolute Minima

--- atcoder/abc127/f/Main.cpp
+++ atcoder/abc127/f/Main.cpp
@@ -21,86 +21,53 @@ using namespace std;
 typedef long long int ll;
 typedef pair<int, int> pii;
 typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
 
-const int NONE = 1e9 + 1;
 const int MAX_Q = 2e5;
-const int MAX_BIT_IDX = MAX_Q;
+const int INF = 1e9 + 1;
 
-template <typename T>
-class BIT {
-public:
-    T b[MAX_BIT_IDX + 1];
-    int n;
-    void init(int num) {
-        n = num;
-        for (int i = 0; i <= n; i++) {
-            b[i] = 0;
-        }
-        return;
-    }
-    T sum(int i) {  // i : [0, n), returns sum of [0, i)
-        T s = 0;
-        for (; i > 0; i -= i & -i) {
-            s += b[i];
-        }
-        return s;
-    }
-    void add(int i, T x) {  // i : [0, n)
-        for (i += 1; i <= n; i += i & -i) {
-            b[i] += x;
-        }
-        return;
-    }
+struct Query {
+    bool ty;
+    int a, b;
 };
 
 int Q;
-bool ty[MAX_Q];
-int a[MAX_Q], b[MAX_Q];
-BIT<ll> bit1, bit2;
+Query qs[MAX_Q];
+multiset<int> ls = {-INF}, rs = {INF};
+
+pll update(Query &q, ll fx) {
+    int lx = *ls.rbegin();
+    int rx = (ls.size() == rs.size()) ? *rs.begin() : lx;
+    if (q.a < lx) {
+        fx += lx - q.a;
+    } else if (rx < q.a) {
+        fx += q.a - rx;
+    }
+    fx += q.b;
+
+    if (ls.size() > rs.size()) {
+        rs.insert(q.a);
+    } else {
+        ls.insert(q.a);
+    }
+    auto lr = *ls.rbegin(), rl = *rs.begin();
+    if (lr > rl) {
+        ls.erase(--ls.end());
+        rs.erase(rs.begin());
+        ls.insert(rl);
+        rs.insert(lr);
+    }
+
+    return {*ls.rbegin(), fx};
+}
 
 void solve() {
-    vector<int> ca(Q);
+    pll ans = {0, 0};
     for (int i = 0; i < Q; i++) {
-        ca[i] = a[i];
-    }
-    sort(ca.begin(), ca.end());
-    ca.erase(unique(ca.begin(), ca.end()), ca.end());
-    for (int i = 0; i < Q; i++) {
-        if (a[i] != NONE) {
-            a[i] = lower_bound(ca.begin(), ca.end(), a[i]) - ca.begin();
-        }
-    }
-
-    bit1.init(Q);
-    bit2.init(Q);
-    int cnt = 0;
-    ll sb = 0;
-    for (int i = 0; i < Q; i++) {
-        if (ty[i]) {
-            // answer
-            int ng = -1;
-            int ok = Q;
-            while (ok - ng > 1) {
-                int m = (ok + ng) / 2;
-                if (bit1.sum(m + 1) >= (cnt + 1) / 2) {
-                    ok = m;
-                } else {
-                    ng = m;
-                }
-            }
-
-            int x = ca[ok];
-            ll cnt_x = bit1.sum(ok) - (bit1.sum(Q) - bit1.sum(ok));
-            ll sa = -bit2.sum(ok) + (bit2.sum(Q) - bit2.sum(ok));
-            ll fx = sb + 1LL * x * cnt_x + sa;
-
-            cout << x << " " << fx << endl;
+        if (qs[i].ty) {
+            cout << ans.first << " " << ans.second << endl;
         } else {
-            // update
-            cnt++;
-            bit1.add(a[i], 1);
-            bit2.add(a[i], ca[a[i]]);
-            sb += b[i];
+            ans = update(qs[i], ans.second);
         }
     }
 }
@@ -110,11 +77,9 @@ int main() {
     for (int i = 0; i < Q; i++) {
         int tmp_ty;
         cin >> tmp_ty;
-        ty[i] = (tmp_ty == 2);
-        if (ty[i]) {
-            a[i] = b[i] = NONE;
-        } else {
-            cin >> a[i] >> b[i];
+        qs[i].ty = (tmp_ty == 2);
+        if (!qs[i].ty) {
+            cin >> qs[i].a >> qs[i].b;
         }
     }
     solve();
