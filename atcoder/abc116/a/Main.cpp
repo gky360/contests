6cac52e4521fd37be6ef53eabf147260799e9e3f
gky360 <gky360@gmail.com>
2019-07-16 12:09:36 +0900

    [abc116] A - Right Triangle

---
+++ atcoder/abc116/a/Main.cpp
@@ -0,0 +1,39 @@
+/*
+[abc116] A - Right Triangle
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+
+int a, b, c;
+
+ll solve() {
+    double s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+int main() {
+    cin >> a >> b >> c;
+
+    cout << solve() << endl;
+
+    return 0;
+}
