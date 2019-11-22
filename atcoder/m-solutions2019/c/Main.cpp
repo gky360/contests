7d214102ea44daa5dbb846916f7cb45e5adafe95
gky360 <gky360@gmail.com>
2019-11-22 14:52:08 +0900

    [m-solutions2019] C - Best-of-(2n-1)

--- atcoder/m-solutions2019/c/Main.cpp
+++ atcoder/m-solutions2019/c/Main.cpp
@@ -10,57 +10,64 @@ typedef pair<ll, int> pli;
 typedef pair<ll, ll> pll;
 
 const int MAX_N = 100000;
-const ll MOD = 1e9 + 7;
+const int MOD = 1e9 + 7;
+
+class gf {
+public:
+    int n;
+    static int extgcd(int a, int b, int& x, int& y) {
+        int d = a;
+        if (b != 0) {
+            d = extgcd(b, a % b, y, x);
+            y -= (a / b) * x;
+        } else {
+            x = 1;
+            y = 0;
+        }
+        return d;
+    }
+    static int mod_inv(int a, int m) {
+        int x, y;
+        extgcd(a, m, x, y);
+        return (m + x) % m;
+    }
+    gf() : n(0) {}
+    gf(int n) : n(n % MOD) {}
+    gf inv() { return gf(mod_inv(n, MOD)); }
+    gf operator+(gf x) { return gf((n + x.n) % MOD); }
+    gf operator-(gf x) { return gf((n - x.n + MOD) % MOD); }
+    gf operator*(gf x) { return gf((1LL * n * x.n) % MOD); }
+    gf operator/(gf x) { return *this * x.inv(); }
+};
 
 int N, A, B, C;
-ll pa[2 * MAX_N + 1], pb[2 * MAX_N + 1], ph[2 * MAX_N + 1];
-ll fact[2 * MAX_N + 1];
-
-ll extgcd(ll a, ll b, ll& x, ll& y) {
-    ll d = a;
-    if (b != 0) {
-        d = extgcd(b, a % b, y, x);
-        y -= (a / b) * x;
-    } else {
-        x = 1;
-        y = 0;
-    }
-    return d;
-}
-
-ll mod_inv(ll a, ll m) {
-    ll x, y;
-    extgcd(a, m, x, y);
-    return (m + x) % m;
-}
+gf pa[2 * MAX_N + 1], pb[2 * MAX_N + 1], ph[2 * MAX_N + 1];
+gf fact[2 * MAX_N + 1];
 
 ll solve() {
+    fact[0] = fact[1] = 1;
+    for (int i = 2; i <= 2 * MAX_N; i++) {
+        fact[i] = fact[i - 1] * i;
+    }
+
     pa[0] = 1;
     pb[0] = 1;
     ph[0] = 1;
     for (int i = 1; i <= 2 * N; i++) {
-        pa[i] = (pa[i - 1] * A) % MOD;
-        pb[i] = (pb[i - 1] * B) % MOD;
-        ph[i] = (ph[i - 1] * (100 - C)) % MOD;
+        pa[i] = pa[i - 1] * A;
+        pb[i] = pb[i - 1] * B;
+        ph[i] = (ph[i - 1] / (100 - C));
     }
 
-    fact[0] = 1;
-    for (int i = 1; i <= 2 * N; i++) {
-        fact[i] = (fact[i - 1] * i) % MOD;
-    }
-
-    ll ans = 0;
+    gf ans = 0;
     for (int i = N; i <= 2 * N - 1; i++) {
-        ll tmp =
-            (fact[i - 1] * mod_inv(fact[N - 1] * fact[i - N] % MOD, MOD)) % MOD;
-        (tmp *= (((pa[N] * pb[i - N] % MOD + pa[i - N] * pb[N] % MOD) % MOD) *
-                 i * 100) %
-                MOD) %= MOD;
-        (tmp *= mod_inv(ph[i + 1] % MOD, MOD)) %= MOD;
-        (ans += tmp) %= MOD;
+        gf tmp = fact[i - 1] / (fact[N - 1] * fact[i - N]);
+        tmp = tmp * (pa[N] * pb[i - N] + pa[i - N] * pb[N]) * i * 100;
+        tmp = tmp * ph[i + 1];
+        ans = ans + tmp;
     }
 
-    return ans;
+    return ans.n;
 }
 
 int main() {
