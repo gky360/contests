10713b3a56f421c93e8dd7d5358ba318721f47ac
gky360 <gky360@gmail.com>
2019-11-22 14:53:58 +0900

    fix templates

--- templates/gcd.cpp
+++ templates/gcd.cpp
@@ -33,4 +33,50 @@ ll mod_inv(ll a, ll m) {
     return (m + x) % m;
 }
 
-int main() { return 0; }
+const int MAX_N = 1e5;
+const int MOD = 1e9 + 7;
+
+// galois field
+class gf {
+public:
+    int n;
+    static int extgcd(int a, int b, int& x, int& y) {
+        int d = a;
+        if (b != 0) {
+            d = extgcd(b, a % b, y, x);
+            y -= (a / b) * x;
+        } else {
+            x = 1;
+            y = 0;
+        }
+        return d;
+    }
+    static int mod_inv(int a, int m) {
+        int x, y;
+        extgcd(a, m, x, y);
+        return (m + x) % m;
+    }
+    gf() : n(0) {}
+    gf(int n) : n(n % MOD) {}
+    gf inv() { return gf(mod_inv(n, MOD)); }
+    gf operator+(gf x) { return gf((n + x.n) % MOD); }
+    gf operator-(gf x) { return gf((n - x.n + MOD) % MOD); }
+    gf operator*(gf x) { return gf((1LL * n * x.n) % MOD); }
+    gf operator/(gf x) { return *this * x.inv(); }
+};
+
+ll fact[MAX_N + 1], finv[MAX_N + 1], inv[MAX_N + 1];
+
+int main() {
+    // calc fact and inv fact
+    fact[0] = fact[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i <= MAX_N; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = (finv[i - 1] * inv[i]) % MOD;
+    }
+
+    return 0;
+}
