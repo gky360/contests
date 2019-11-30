7e7d9045082894bfdb14643fa717b65749314f17
gky360 <gky360@gmail.com>
2019-11-30 17:49:55 +0900

    fix template

--- templates/min_cost_flow.cpp
+++ templates/min_cost_flow.cpp
@@ -2,56 +2,58 @@
 // 最小費用流を求める
 // グラフは隣接リスト型
 
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
-#include <vector>
 #include <queue>
 #include <utility>
-#define MAX_V 100
-#define INF 100000000
+#include <vector>
 using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
 
+const int MAX_V = 10000;
+const ll INF = 1e18;
 
-template <typename T>
-struct edge{
+struct edge {
     int to, cap;
-    T cost;
+    ll cost;
     int rev;
 };
 
-template <typename T>
-class Graph{
+class Graph {
 public:
-    void add_edge(int from, int to, int cap, T cost);
-    T min_cost_flow(int s, int t, int f);
+    void add_edge(int from, int to, int cap, ll cost);
+    ll min_cost_flow(int s, int t, int f);
+
 private:
-    vector<edge<T>> g[MAX_V];
-    T h[MAX_V];
-    T dist[MAX_V];
+    vector<edge> g[MAX_V];
+    ll h[MAX_V];
+    ll dist[MAX_V];
     int pre_v[MAX_V], pre_e[MAX_V];
 };
 
-template <typename T>
-void Graph<T>::add_edge(int from, int to, int cap, T cost) {
-    g[from].push_back((edge<T>){to, cap, cost, (int)g[to].size()});
-    g[to].push_back((edge<T>){from, 0, -cost, (int)g[from].size() - 1});
+void Graph::add_edge(int from, int to, int cap, ll cost) {
+    g[from].push_back((edge){to, cap, cost, (int)g[to].size()});
+    g[to].push_back((edge){from, 0, -cost, (int)g[from].size() - 1});
     return;
 }
 
-template <typename T>
-T Graph<T>::min_cost_flow(int s, int t, int f) {
-    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> que;
+ll Graph::min_cost_flow(int s, int t, int f) {
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>>
+        que;
     // first: tmp min distance, second: vertex index
-    pair<T, int> p;
+    pair<ll, int> p;
     int d;
-    T cost = 0;
+    ll cost = 0;
 
     memset(h, 0, sizeof(h));
     while (f > 0) {
         fill(dist, dist + MAX_V, INF);
         dist[s] = 0;
-        que.push(pair<T, int>(0, s));
+        que.push(pair<ll, int>(0, s));
         while (!que.empty()) {
             p = que.top();
             que.pop();
@@ -60,12 +62,13 @@ T Graph<T>::min_cost_flow(int s, int t, int f) {
                 continue;
             }
             for (int i = 0; i < (int)g[v].size(); i++) {
-                edge<T> e = g[v][i];
-                if (e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]){
+                edge e = g[v][i];
+                if (e.cap > 0 &&
+                    dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
                     dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
                     pre_v[e.to] = v;
                     pre_e[e.to] = i;
-                    que.push(pair<T, int>(dist[e.to], e.to));
+                    que.push(pair<ll, int>(dist[e.to], e.to));
                 }
             }
         }
@@ -84,7 +87,7 @@ T Graph<T>::min_cost_flow(int s, int t, int f) {
         f -= d;
         cost += h[t] * d;
         for (int v = t; v != s; v = pre_v[v]) {
-            edge<T> &e = g[pre_v[v]][pre_e[v]];
+            edge &e = g[pre_v[v]][pre_e[v]];
             e.cap -= d;
             g[v][e.rev].cap += d;
         }
@@ -92,8 +95,4 @@ T Graph<T>::min_cost_flow(int s, int t, int f) {
     return cost;
 }
 
-
-int main(){
-    return 0;
-}
-
+int main() { return 0; }
