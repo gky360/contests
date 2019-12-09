ca764755f9b64f4fe95773dda1c6f22bb9089208
gky360 <gky360@gmail.com>
2019-12-09 22:37:40 +0900

    fix templates

--- templates/mod.cpp
+++ templates/mod.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 typedef long long int ll;
 
-ll gcd(ll a, ll b) {
-    while (b > 0) {
-        swap(b, a %= b);
-    }
-    return a;
-}
-
-ll lcm(ll a, ll b) {
-    ll d = gcd(a, b);
-    return (ll)a * b / d;
-}
-
 ll extgcd(ll a, ll b, ll& x, ll& y) {
     ll d = a;
     if (b != 0) {
