0a053b2ee3b541f8bcf55da79dca7d8232326bff
gky360 <gky360@gmail.com>
2020-11-21 15:55:48 +0900

    [abc161] A - ABC Swap

---
+++ atcoder/abc161/a/Main.cpp
@@ -0,0 +1,25 @@
+/*
+[abc161] A - ABC Swap
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double DD;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(c) (c).begin(), (c).end()
+
+int X, Y, Z;
+
+void solve() { cout << Z << " " << X << " " << Y; }
+
+int main() {
+    cin >> X >> Y >> Z;
+
+    solve();
+
+    return 0;
+}
