d385bb54a77877572a43338e53fe5e51af8d3585
gky360 <gky360@gmail.com>
2021-02-15 19:35:47 +0900

    [abc172] C - Tsundoku

---
+++ atcoder/abc172/c/Main.cpp
@@ -0,0 +1,30 @@
+/*
+[abc172] C - Tsundoku
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double DD;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(c) (c).begin(), (c).end()
+
+
+ll solve() {
+
+
+    ll ans = 0;
+    return ans;
+}
+
+int main() {
+
+
+    cout << solve() << endl;
+
+    return 0;
+
+}
