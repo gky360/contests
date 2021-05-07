804c91273e02319e28aecd4d14e2a11f30995747
gky360 <gky360@gmail.com>
2021-05-08 01:35:57 +0900

    [abl] A - Repeat ACL

---
+++ atcoder/abl/a/Main.cpp
@@ -0,0 +1,28 @@
+/*
+[abl] A - Repeat ACL
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double DD;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(c) (c).begin(), (c).end()
+
+int K;
+
+void solve() {
+    REP(i, K) cout << "ACL";
+    cout << endl;
+}
+
+int main() {
+    cin >> K;
+
+    solve();
+
+    return 0;
+}
