bd5f7029ecb65975cb0ea102ea14f1b803ccb551
gky360 <gky360@gmail.com>
2021-09-24 22:50:26 +0900

    [arc110] D - Binomial Coefficient is Fun

---
+++ atcoder/arc110/d/Main.cpp
@@ -0,0 +1,65 @@
+/*
+[arc110] D - Binomial Coefficient is Fun
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double DD;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(c) (c).begin(), (c).end()
+
+const int MAX_N = 2000;
+const int MOD = 1e9 + 7;
+
+class gf {
+public:
+    int n;
+    static gf mod_inv(int a, int m) {
+        int u = 0, v = 1;
+        while (a != 0) {
+            int t = m / a;
+            m -= t * a, u -= t * v;
+            swap(a, m), swap(u, v);
+        }
+        return gf(u);
+    }
+    gf() : n(0) {}
+    gf(ll n) : n(n < 0 ? n % MOD + MOD : n % MOD) {}
+    gf operator+(gf x) { return gf((n + x.n) % MOD); }
+    gf operator-(gf x) { return gf((n - x.n + MOD) % MOD); }
+    gf operator*(gf x) { return gf((1LL * n * x.n) % MOD); }
+    gf operator/(gf x) { return *this * x.inv(); }
+    gf& operator+=(gf x) { return *this = (*this + x); }
+    gf& operator-=(gf x) { return *this = (*this - x); }
+    gf& operator*=(gf x) { return *this = (*this * x); }
+    gf& operator/=(gf x) { return *this = (*this / x); }
+    gf inv() { return mod_inv(n, MOD); }
+};
+
+int N, M;
+int A[MAX_N];
+
+ll solve() {
+    int s = accumulate(A, A + N, 0);
+    if (M < s) return 0;
+
+    gf ans = 1;
+    REP(i, s + N) ans *= i + 1;
+    ans = gf(1) / ans;
+    REP(i, s + N) ans *= M + N - i;
+
+    return ans.n;
+}
+
+int main() {
+    cin >> N >> M;
+    REP(i, N) cin >> A[i];
+
+    cout << solve() << endl;
+
+    return 0;
+}
