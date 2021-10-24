3a77d7531031607a0b475aa5e4bdd98b98dbf475
gky360 <gky360@gmail.com>
2021-10-24 23:36:58 +0900

    [agc051] A - Dodecagon

---
+++ atcoder/agc051/a/Main.cpp
@@ -0,0 +1,66 @@
+/*
+[agc051] A - Dodecagon
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef long double DD;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(c) (c).begin(), (c).end()
+
+const int MAX_D = 1e6;
+const int MOD = 998244353;
+
+class gf {
+public:
+    int n;
+    static gf mod_inv(int a, int m) {
+        int u = 0, v = 1;
+        while (a != 0) {
+            int t = m / a;
+            m -= t * a, u -= t * v;
+            swap(a, m), swap(u, v);
+        }
+        return gf(u);
+    }
+    gf() : n(0) {}
+    gf(ll n) : n(n < 0 ? n % MOD + MOD : n % MOD) {}
+    gf operator+(gf x) { return gf((n + x.n) % MOD); }
+    gf operator-(gf x) { return gf((n - x.n + MOD) % MOD); }
+    gf operator*(gf x) { return gf((1LL * n * x.n) % MOD); }
+    gf operator/(gf x) { return *this * x.inv(); }
+    gf& operator+=(gf x) { return *this = (*this + x); }
+    gf& operator-=(gf x) { return *this = (*this - x); }
+    gf& operator*=(gf x) { return *this = (*this * x); }
+    gf& operator/=(gf x) { return *this = (*this / x); }
+    gf inv() { return mod_inv(n, MOD); }
+};
+
+gf fact[2 * MAX_D + 1];
+
+gf combi(int n, int k) {
+    return (n < 0 || k < 0 || n - k < 0) ? 0
+                                         : fact[n] / (fact[k] * fact[n - k]);
+}
+
+int d;
+
+ll solve() {
+    fact[0] = 1;
+    REP(i, 2 * d) fact[i + 1] = fact[i] * (i + 1);
+
+    gf ans = combi(d + d, d) / 2;
+    return ans.n;
+}
+
+int main() {
+    cin >> d;
+
+    cout << solve() << endl;
+
+    return 0;
+}
